d8348fcd84776e87ee6504d6635eb9d54f46be7a
vv <vv@VV-DOM>
2022-11-06 09:59:26 +0300

    Добавил классы для робтп breakdown

---
+++ OsEngine/ViewModels/RobotBreakVM.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using OsEngine.Commands;
+using OsEngine.Entity;
+using OsEngine.Market;
+using OsEngine.Market.Servers;
+using OsEngine.Market.Servers.GateIo.Futures.Response;
+using OsEngine.MyEntity;
+using OsEngine.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Action = OsEngine.MyEntity.Action;
+using Direction = OsEngine.MyEntity.Direction;
+
+namespace OsEngine.ViewModels
+{
+    public class RobotBreakVM : BaseVM
+    {
+
+    }
+}
